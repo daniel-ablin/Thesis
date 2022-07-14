d26c69cb3917389f6b5e06857fc3f2a03ab97857
daniel-ablin <78722263+daniel-ablin@users.noreply.github.com>
2022-07-14 08:32:03 +0300

    startup
    startup

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def iteration(x, t, v, outer):
+    for i in range(len(x):
+        new_x = np.zeros(2))
+        for j in v.shape[0]:
+            new_x[i] = x[i] + outer['beta']*[outer['d'][0,0] - v[0,0]]

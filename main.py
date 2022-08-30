64066887db50aa3a644e593d6b8076f5ea826d3c
Daniel Ablin <daniel.avlin@caliente.mx>
2022-08-30 20:37:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ for itr in range(5000):
     print(f'\niteration: {itr}')
     start = timer()
     T = rnd.integers(1, 1000)
-    T = 300
     I0 = (0.1 - epsilon) * rnd.random() + epsilon
     temp = rnd.integers(1, 100)
     outer = {'beta': 2.3/30,
@@ -41,11 +40,11 @@ for itr in range(5000):
 
     sol = optimize(T, I0, outer, gov=False, learning_rate=.01, max_itr=10000, epsilon=10**-8, beta_1=.9
                    , beta_2=.999, Recovered_rate=0, ReSusceptible_rate=0, stop_itr=50, threshold=Threshold
-                   , only_finals=True, seed=seed)
+                   , only_finals=True, seed=seed, derv_test=True, solution_test=True)
 
     sol_gov = optimize(T, I0, outer, gov=True, learning_rate=.01, max_itr=10000, epsilon=10**-8, beta_1=.9
                        , beta_2=.999, Recovered_rate=0, ReSusceptible_rate=0, stop_itr=50, threshold=Threshold
-                       , only_finals=True, seed=seed)
+                       , only_finals=True, seed=seed, derv_test=True, solution_test=True)
 
     end = timer()
     data.append([T, I0, outer['d'], outer['l'], Recovered_rate, ReSusceptible_rate, sol, sol_gov, end - start])

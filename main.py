6d84898ae7f47f4e42d146484dc862e13c3b7a00
Daniel Ablin <daniel.avlin@caliente.mx>
2022-08-31 11:14:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ seed = 20
 columns = ['T', 'I0', 'd', 'l', 'Recovered_rate', 'ReSusceptible_rate', 'sol', 'sol_gov', 'time']
 data = list()
 rnd = np.random.default_rng(seed)
-for itr in range(5000):
+for itr in range(100):
     print(f'\niteration: {itr}')
     start = timer()
     T = rnd.integers(1, 1000)
@@ -40,11 +40,11 @@ for itr in range(5000):
 
     sol = optimize(T, I0, outer, gov=False, learning_rate=.01, max_itr=10000, epsilon=10**-8, beta_1=.9
                    , beta_2=.999, Recovered_rate=0, ReSusceptible_rate=0, stop_itr=50, threshold=Threshold
-                   , only_finals=True, seed=seed, derv_test=True, solution_test=True)
+                   , seed=seed, derv_test=True, solution_test=True)
 
     sol_gov = optimize(T, I0, outer, gov=True, learning_rate=.01, max_itr=10000, epsilon=10**-8, beta_1=.9
                        , beta_2=.999, Recovered_rate=0, ReSusceptible_rate=0, stop_itr=50, threshold=Threshold
-                       , only_finals=True, seed=seed, derv_test=True, solution_test=True)
+                       , seed=seed, derv_test=True, solution_test=True)
 
     end = timer()
     data.append([T, I0, outer['d'], outer['l'], Recovered_rate, ReSusceptible_rate, sol, sol_gov, end - start])

ec5cb0c79402171142c23c9fa044796fd004b9c6
Daniel Ablin <daniel.avlin@caliente.mx>
2022-09-06 08:49:15 +0300

    Update func.py

--- utils/func.py
+++ utils/func.py
@@ -9,7 +9,7 @@ def infected(I, v, outer):
     new_I = (outer['beta'] * outer['d'] * v * v.T * (1 - I).reshape((outer['d'].shape[0], 1)) *
              I.reshape((1, outer['d'].shape[0]))).sum(axis=1)
 
-    return I + new_I
+    return I + new_I[:, np.newaxis]
 
 
 def init_params(max_itr, groups, gov, one_v_for_all, seed):
@@ -28,7 +28,7 @@ def init_params(max_itr, groups, gov, one_v_for_all, seed):
 
 def optimize(T, I0, outer, gov=False, one_v_for_all=False, learning_rate=.01, max_itr=10000, epsilon=10**-8, beta_1=.9, beta_2=.999
              , Recovered_rate=0, ReSusceptible_rate=0, stop_itr=50, threshold=10**-6, seed=None
-             , derv_test=False, solution_test=False):
+             , derv_test=False, solution_test=False, total_cost_test=False):
     m = 0
     u = 0
     groups = outer['d'].shape[0]
@@ -39,10 +39,10 @@ def optimize(T, I0, outer, gov=False, one_v_for_all=False, learning_rate=.01, ma
     pbar = tqdm(range(max_itr))
     for itr in pbar:
         dI = np.zeros(groups) if gov else np.zeros((groups, groups)) if one_v_for_all else np.zeros((groups, groups, groups))
-        I = np.zeros((T, groups))
+        I = np.zeros((T, groups, 1))
         I[0, :] = I0
 
-        if derv_test or solution_test:
+        if derv_test or solution_test or total_cost_test:
             test_results = dict()
             I_test = I[0, :].copy()
             v_test = v[itr].copy()
@@ -62,7 +62,7 @@ def optimize(T, I0, outer, gov=False, one_v_for_all=False, learning_rate=.01, ma
 
         for t in range(T-1):
             I[t + 1, :] = infected(I[t, :], v[itr], outer)
-            if derv_test or solution_test:
+            if derv_test or solution_test or total_cost_test:
                 I_test = infected(I_test, v_test, outer)
             if Recovered_rate > 0:
                 R[t + 1, :] = I[t, :] * Recovered_rate
@@ -78,12 +78,20 @@ def optimize(T, I0, outer, gov=False, one_v_for_all=False, learning_rate=.01, ma
 
             derv = dI_agg[main_player_test] if gov or one_v_for_all else dI_agg[main_player_test, sub_player_test]
 
-            test_results['derv'] = (abs(derv - dv_test) < epsilon*10) and test_results.get('derv', True)
+            test_results['derv'] = (abs(derv - dv_test) < epsilon*100) and test_results.get('derv', True)
 
         dCost = -1 / v[itr] ** 2
 
         TotalCost[itr] = (outer['l'].reshape(groups, 1) * I[T - 1] + 1 / v[itr] - 1).sum(axis=1)
         dTotalCost[itr] = outer['l'].reshape(groups, 1) * dI_agg + dCost
+        if total_cost_test:
+            TotalCost_test = (outer['l'][main_player_test] * I_test[main_player_test] + 1 / v_test[main_player_test] - 1)
+            cost = TotalCost[itr][main_player_test] if gov or one_v_for_all else TotalCost[itr][main_player_test, sub_player_test]
+            dTotalCost_test = (TotalCost_test - cost)/epsilon
+            cost_derv = dTotalCost[itr][main_player_test] if gov or one_v_for_all else dTotalCost[itr][main_player_test, sub_player_test]
+
+            test_results['cost_derv'] = (abs(cost_derv - dTotalCost_test) < epsilon * 1000) and test_results.get('cost_derv', True)
+
         grad = dTotalCost[itr].sum() if gov else dTotalCost[itr]
         decent, m, u = adam_optimizer_iteration(grad, m, u, beta_1, beta_2, itr, epsilon,
                                                 learning_rate) # / (floor(itr/1000) + 1))
